d5bbf0d5e3e69b6371e233ef757c50854f12cf90
Initio-SIlentium-Noctis <wolf495@gmail.com>
2025-03-09 15:41:48 +0300

    - initial commit

---
+++ src/main/java/net/Non/villageguardian/VillageGuardian.java
@@ -0,0 +1,29 @@
+package net.Non.villageguardian;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VillageGuardian implements ModInitializer, ClientModInitializer {
+	public static final String MOD_ID = "village_guardian";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+
+
+
+
+
+}
